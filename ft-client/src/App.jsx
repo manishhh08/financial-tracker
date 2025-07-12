c97741fdb4dec6c9344ed2d63000fcfd93fa0ee5
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-07-12 22:57:11 +1000

    login update

--- ft-client/src/App.jsx
+++ ft-client/src/App.jsx
@@ -2,12 +2,13 @@ import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="container">
-        hello world
+      <div className="container-fluid d-flex flex-direction-column align-items-center justify-content-center bg-dark text-white vw-100% vh-100%">
+        afdas
         <Login />
       </div>
     </>
