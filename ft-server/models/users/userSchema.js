052ea47a46187e305f618f6bfa266ff3bd3813d2
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-07-28 22:41:08 +1000

    user schema update

--- ft-server/models/users/userSchema.js
+++ ft-server/models/users/userSchema.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
