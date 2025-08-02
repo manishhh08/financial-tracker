0afa1cb58d0ddd71a089ba45bc07d8316b7b5d88
manishhh08 <100024795+manishhh08@users.noreply.github.com>
2025-08-02 23:57:23 +1000

    model and schema update

--- ft-server/models/users/userSchema.js
+++ ft-server/models/users/userSchema.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    status: {
+      type: Boolean,
+      default: false,
+    },
     username: {
       type: String,
       required: true,
